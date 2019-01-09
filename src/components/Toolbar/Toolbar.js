2c93354f1ff32f516933f4182ad1c8645b85ef58
Raymond Kirkland <35280589+Rizz311@users.noreply.github.com>
2019-01-08 20:42:21 -0500

    Add files via upload

---
+++ src/components/Toolbar/Toolbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const toolbar = props => (
+<header>
+<nav>
+    <div>
+        <div><a href='/'></a></div>
+        <div>
+            <ul>
+            <li><a href='/'>Product</a></li>
+            <li><a href='/'>Users</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+</header>        
+
+);
+
+export default toolbar;
